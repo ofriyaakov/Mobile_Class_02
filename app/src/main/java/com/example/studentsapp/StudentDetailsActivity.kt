3bed0945f78db594f8a2068e9f248ddbafc82b4a
Ofri Yaakov <ofriyaakov@gmail.com>
2025-01-21 23:46:31 +0200

    add activity functionality

--- app/src/main/java/com/example/studentsapp/StudentDetailsActivity.kt
+++ app/src/main/java/com/example/studentsapp/StudentDetailsActivity.kt
@@ -1,10 +1,13 @@
 package com.example.studentsapp
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.studentsapp.model.Student
 
 class StudentDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,27 @@ class StudentDetailsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val student = getStudentDataFromIntent()
+        setStudentDetails(student)
+    }
+
+    private fun getStudentDataFromIntent() : Student? {
+        val student: Student? = intent.getParcelableExtra("student")
+        return student
+    }
+
+    private fun setStudentDetails(student: Student?) {
+        val nameValue: TextView = findViewById(R.id.nameValue)
+        val idValue: TextView = findViewById(R.id.idValue)
+        val phoneValue: TextView = findViewById(R.id.phoneValue)
+        val addressValue: TextView = findViewById(R.id.addressValue)
+        val isChecked: CheckBox = findViewById(R.id.checkBox)
+
+        nameValue.text = student?.name
+        idValue.text = student?.id
+        phoneValue.text = student?.phone
+        addressValue.text = student?.address
+        isChecked.isChecked = student?.isChecked ?: false
     }
 }
